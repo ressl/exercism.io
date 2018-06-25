1d5c81c83e1eb7a4a224eb4a9178e228ec6697d8
Ressl Robert <r.ressl@safematix.com>
2018-06-25 21:23:28 +0200

    :white_check_mark: add exercism.io go acronym

---
+++ go/acronym/acronym.go
@@ -0,0 +1,18 @@
+package acronym
+
+import (
+	"strings"
+)
+
+// Abbreviate create a acronym from each string
+func Abbreviate(s string) string {
+	words := strings.Split(strings.Join(strings.Split(s, " "), "-"), "-")
+
+	acronym := ""
+
+	for _, word := range words {
+		acronym = acronym + string([]rune(word)[0])
+	}
+
+	return (strings.ToUpper(acronym))
+}
